2793e7c6f5e442daf505cb383bdfaad84d1ac681
GPINI98 <pini.work98@gmail.com>
2026-08-31 16:21:08 +0200

    4. Separato funzioni in file config ed aggiunto programma main

---
+++ config.py
@@ -0,0 +1,39 @@
+# DEFINIZIONE FUNZIONI 
+# ################# MOSTRA DIPENDENTI
+def mostra_dipendenti(dipendenti):
+    for dipendente in dipendenti:
+        print(f"{dipendente['nome']}, {dipendente['eta']} anni , {dipendente['stipendio']} euro")
+
+# ################# CERCA DIPENDENTI
+def cerca_dipendente(dipendenti, nome):
+    for dipendente in dipendenti:
+        if dipendente['nome'] == nome:
+            return dipendente
+    return None
+
+# ################# MOSTRA MASSIMO STIPENDIO
+def trova_massimo(dipendenti):
+    dipendente_max = None
+    for dipendente in dipendenti:
+        if dipendente_max is None or dipendente_max ['stipendio'] < dipendente ['stipendio']:
+            dipendente_max = dipendente
+    return dipendente_max
+
+# ################# AGGIUNGI DIPENDENTE
+def aggiungi_dipendente(dipendenti, nome, eta, stipendio):
+    nuovo_dipendente = {
+        "nome" : nome,
+        "eta" : eta,
+        "stipendio" : stipendio
+    }
+    dipendenti.append(nuovo_dipendente)
+
+# ################# ELIMINA DIPENDENTE
+def elimina_dipendente(dipendenti):
+    nome_richiesto = input("che nome devo cercare?")
+    risultato = cerca_dipendente(dipendenti, nome_richiesto) 
+    if risultato is None:
+        print("dipendente non trovato")
+    else :
+        dipendenti.remove(risultato)
+    print(f"{nome_richiesto} è stato eliminato")
